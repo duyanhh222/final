4594903775af93efcfa231e138980b559f08dbac
duyanhh222 <84457760+duyanhh222@users.noreply.github.com>
2023-07-04 12:54:15 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
-
+var_dump('5555');
+die();
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
